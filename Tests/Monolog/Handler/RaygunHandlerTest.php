781d0eeabef089b6a7bfdec1f63642f257aef5a4
nietonfir <nietonfir@gmail.com>
2016-01-26 01:10:32 +0100

    Add missing copyright statements in some of the files

--- Tests/Monolog/Handler/RaygunHandlerTest.php
+++ Tests/Monolog/Handler/RaygunHandlerTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Raygunbundle package.
+ *
+ * (c) nietonfir <nietonfir@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Nietonfir\RaygunBundle\Tests\Monolog\Handler;
 
 use Monolog\TestCase;
