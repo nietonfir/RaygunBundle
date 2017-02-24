36f3e5d60e0e7df5330b505df1c49e2de27cb896
Matteo B <matteo@belka.us>
2017-02-24 10:52:51 +0100

    initial dev commit pasted edited code from auth

---
+++ Monolog/Handler/Client.php
@@ -0,0 +1,27 @@
+<?php
+namespace Nietonfir\RaygunBundle\Monolog\Handler;
+
+use Raygun4php\RaygunClient;
+
+class Client extends RaygunClient
+{
+    private $defaultTags = null;
+
+    public function setDefaultTags(array $tags)
+    {
+        $this->defaultTags = $tags;
+    }
+
+    public function SendError($errno, $errstr, $errfile, $errline, $tags = null, $userCustomData = null, $timestamp = null)
+    {
+        if (is_array($this->defaultTags)) {
+            if (is_array($tags)) {
+                $tags = array_merge($this->defaultTags, $tags);
+            } else {
+                $tags = $this->defaultTags;
+            }
+        }
+
+        return parent::SendError($errno, $errstr, $errfile, $errline, $tags, $userCustomData, $timestamp);
+    }
+}
